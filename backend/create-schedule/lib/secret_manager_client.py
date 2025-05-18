d2270568f24b0353798dba4e3d2bb6e337fab6d9
KoyanagiAyuha <ayuha.koyanagi@gmail.com>
2025-05-19 03:50:12 +0900

    update: シークレットのボリュームマウントをAPI経由の取得に変更

--- backend/create-schedule/lib/secret_manager_client.py
+++ backend/create-schedule/lib/secret_manager_client.py
@@ -1,4 +1,6 @@
 from google.cloud import secretmanager
+import os
+import tempfile
 
 
 def get_openweathermap_api_key():
@@ -13,3 +15,19 @@ def get_gemini_api_key():
     secret_path = "projects/131464926474/secrets/GEMINI_API_KEY/versions/latest"
     response = client.access_secret_version(request={"name": secret_path})
     return response.payload.data.decode("UTF-8")
+
+
+def setup_firebase_credentials_env():
+    # Secret取得
+    client = secretmanager.SecretManagerServiceClient()
+    name = "projects/131464926474/secrets/firebase-credentials/versions/latest"
+    response = client.access_secret_version(request={"name": name})
+    json_str = response.payload.data.decode("UTF-8")
+
+    # 一時ファイルへ保存
+    with tempfile.NamedTemporaryFile(mode="w", suffix=".json", delete=False) as f:
+        f.write(json_str)
+        temp_path = f.name
+
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = temp_path
+    return temp_path

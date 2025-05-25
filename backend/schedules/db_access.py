47dd2c542c1c243aedf22eab7ee4e6e677b69809
asukatoru <wojk684terai@gmail.com>
2025-05-26 05:04:44 +0900

    スケジュール取得のコード作成

--- backend/schedules/db_access.py
+++ backend/schedules/db_access.py
@@ -1,13 +1,16 @@
 import google.cloud.firestore
+from google.cloud.firestore_v1.base_query import FieldFilter, BaseCompositeFilter
 
 def get_schedules_by_user_and_period(user_id, start_time, end_time):
-    db = google.cloud.firestore.Client(database='hisho-events')
+    db = google.cloud.firestore.Client()
+    collection = db.collection('users').document(user_id).collection('events')
     
-    results = {
-        db.collection('users').document('se0e3x3pee4NuXYUlIbf').collection('events')
-        .where("user_id", "==", user_id)
-        .where("start_time", ">=", start_time)
-        .where("end_time", "<=", end_time)
-        .order_by("start_time").stream()
-    }
+    filters = [
+        ("start_time", ">=", start_time),
+        ("start_time", "<", end_time),
+    ]
+
+    field_filters = [FieldFilter(field, op, val) for field, op, val in filters]
+    result = collection.where(filter=BaseCompositeFilter("AND", field_filters)).order_by("start_time").stream()
+
     return result

9303596f9cb3f1efd58714f5195142e60087ec3a
asukatoru <wojk684terai@gmail.com>
2025-05-26 04:07:06 +0900

    スケジュール取得のコード作成

--- backend/schedules/db_access.py
+++ backend/schedules/db_access.py
@@ -3,6 +3,11 @@ import google.cloud.firestore
 def get_schedules_by_user_and_period(user_id, start_time, end_time):
     db = google.cloud.firestore.Client(database='hisho-events')
     
-    results = db.collection('users').document('se0e3x3pee4NuXYUlIbf').collection('events').stream()
-    
+    results = {
+        db.collection('users').document('se0e3x3pee4NuXYUlIbf').collection('events')
+        .where("user_id", "==", user_id)
+        .where("start_time", ">=", start_time)
+        .where("end_time", "<=", end_time)
+        .order_by("start_time").stream()
+    }
     return result
